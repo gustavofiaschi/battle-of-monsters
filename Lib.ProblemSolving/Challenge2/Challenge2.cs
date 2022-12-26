d3d056426199a846deda52a0c9803ad207aee33a
Thiago Galvani <tgalvani@fullstacklabs.co>
2022-12-26 17:52:34 -0300

    fix: adjusting challenge 2 file

--- Lib.ProblemSolving/Challenge2/Challenge2.cs
+++ Lib.ProblemSolving/Challenge2/Challenge2.cs
@@ -4,10 +4,6 @@ public static class Challenge2
 {
     public static int DiceFacesCalculator(int dice1, int dice2, int dice3)
     {
-        if (dice1 > 6 || dice1 < 1)
-        {
-            throw new Exception("Dice out of number range");
-        }
         return 0;
     }
 }
