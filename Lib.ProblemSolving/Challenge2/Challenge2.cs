df5420387fb44a9abb6acad42d49762d3fd5f658
Fernando Sanabria <131177639+FernandoSanabriaFSL@users.noreply.github.com>
2023-09-22 10:16:08 -0500

    v2.0.0
    * v2.0.0
    
    * v2.0.0

--- Lib.ProblemSolving/Challenge2/Challenge2.cs
+++
@@ -1,9 +0,0 @@
-namespace Lib.ProblemSolving;
-
-public static class Challenge2
-{
-    public static int DiceFacesCalculator(int dice1, int dice2, int dice3)
-    {
-        return 0;
-    }
-}
--- Lib.ProblemSolving/Challenge3/Challenge3.cs
+++
@@ -1,9 +0,0 @@
-namespace Lib.ProblemSolving;
-
-public static class Challenge3
-{
-    public static int FindLessCostPath(int[][] board)
-    {
-        return 0;
-    }
-}
